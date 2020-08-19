6c8afdbfdf909d7c7d3a972e77da6a3cbc5192e9
徐小平 <2547128887@qq.com>
2020-08-19 16:51:02 +0800

    进一步修改了增删改查

--- demo0812/src/main/java/com/example/demo0812/Controller/RuleController.java
+++ demo0812/src/main/java/com/example/demo0812/Controller/RuleController.java
@@ -1,53 +1,110 @@
 package com.example.demo0812.Controller;
 
+
+//package com.example.demo0812.Controller;
+//
+//import com.example.demo0812.bean.Rule;
+//import com.example.demo0812.service.RuleService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.List;
+//
+//@Controller
+//public class RuleController {
+//
+//    @Autowired
+//    private RuleService ruleService;
+//
+//    @RequestMapping("rule")
+//    @ResponseBody
+//    public ModelAndView listALL(Model model){
+//
+//        ModelAndView mav = new ModelAndView("rule");
+//        List<Rule> rules=ruleService.listALL();
+//        mav.addObject("rules",rules);
+//        return mav;
+//    }
+//
+//    @GetMapping("{id}")
+//    public List<Rule> ruleQuery(String pcode){
+//        List<Rule> rules=ruleService.ruleQuery("GZ01010101");
+//        for (Rule r:rules){
+//            System.out.println("ruleQuery:"+r.getPname()+ " code:" +r.getPcode());
+//        }
+//        return rules;
+//    }
+//
+//    @RequestMapping(value = "/ruleInsert", method = RequestMethod.POST)
+//    public void ruleInsert(@ModelAttribute(value="rule") Rule rule){
+//        ruleService.ruleInsert(rule);
+//    }
+//
+//    @GetMapping("/ruleUpdate/{pname}+{ppos}")
+//    public void ruleUpdate(@PathVariable("pname") String pname, String ppos){
+//        ruleService.ruleUpdate(pname,ppos);
+//    }
+//
+//    @GetMapping("/ruleDelete/{pname}")
+//    public void ruleDelete(@PathVariable("pname") String pname){
+//        System.out.println(pname);
+//        ruleService.ruleDelete(pname);
+//    }
+//}
 import com.example.demo0812.bean.Rule;
 import com.example.demo0812.service.RuleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
-public class RuleController {
-
+@RestController
+@RequestMapping("/rules")
+public class RuleController{
     @Autowired
     private RuleService ruleService;
 
-    @RequestMapping("rule")
-    @ResponseBody
-    public ModelAndView listALL(Model model){
-
-        ModelAndView mav = new ModelAndView("rule");
-        List<Rule> rules=ruleService.listALL();
-        mav.addObject("rules",rules);
-        return mav;
+    @GetMapping
+    public ModelAndView list(Model model) {
+        model.addAttribute("ruleList", ruleService.listALL());
+        model.addAttribute("title", "规则管理");
+        return new ModelAndView("rules/rule","ruleModel",model);
     }
 
-    @GetMapping("{id}")
-    public List<Rule> ruleQuery(String pcode){
-        List<Rule> rules=ruleService.ruleQuery("GZ01010101");
-        for (Rule r:rules){
-            System.out.println("ruleQuery:"+r.getPname()+ " code:" +r.getPcode());
-        }
-        return rules;
+    @GetMapping("/queryRule/{pcode}")
+    public ModelAndView ruleQuery(@PathVariable("pcode") String pcode,Model model) {
+        List<Rule> rules = ruleService.ruleQuery(pcode);
+        model.addAttribute("ruleList",rules);
+        model.addAttribute("title", "查询规则");
+        return new ModelAndView("rules/queryRule","ruleModel",model);
     }
 
-    @RequestMapping(value = "/ruleInsert", method = RequestMethod.POST)
-    public void ruleInsert(@ModelAttribute(value="rule") Rule rule){
-        ruleService.ruleInsert(rule);
+    @GetMapping("/addRule")
+    public ModelAndView createRule(Model model) {
+        model.addAttribute("rule",new Rule());
+        model.addAttribute("title", "创建规则");
+        return new ModelAndView("rules/addRule","ruleModel",model);
     }
 
-    @GetMapping("/ruleUpdate/{pname}+{ppos}")
-    public void ruleUpdate(@PathVariable("pname") String pname, String ppos){
+    @PostMapping
+    public ModelAndView UpdateRule(String pname, String ppos) {
         ruleService.ruleUpdate(pname,ppos);
+        return new ModelAndView("redirect:/rules");//重定向到list页面
     }
 
     @GetMapping("/ruleDelete/{pname}")
-    public void ruleDelete(@PathVariable("pname") String pname){
-        System.out.println(pname);
+    public ModelAndView ruleDelete(@PathVariable("pname") String pname) {
         ruleService.ruleDelete(pname);
+        return new ModelAndView("redirect:/rules");//重定向到list页面
     }
+
 }
--- demo0812/src/main/java/com/example/demo0812/Controller/indexContller.java
+++ demo0812/src/main/java/com/example/demo0812/Controller/indexContller.java
@@ -1,13 +1,18 @@
 package com.example.demo0812.Controller;
 
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexContller {
     @RequestMapping("")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("pcode", new String());
+
         return "index";
     }
 
